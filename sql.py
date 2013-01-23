144589df466b60a60fbaa240066aaa71bbb1b8fe
Touya Akira <none@none.org>
2013-01-23 08:40:59 +0000

    minor format fix

--- sql.py
+++ sql.py
@@ -73,7 +73,7 @@ def deli(phenny, input):
 	if (input.sender != CCHANNEL): return
 	else:
 		if len(input.group(2)) != 64:
-			phenny.say("Hash length does not match. Are you sure you have       entered a sha256 hash?")
+			phenny.say("Hash length does not match. Are you sure you have entered a sha256 hash?")
 			return
 		else:
 			db = MySQLdb.connect(host=THOST, user=TUSER, passwd=TPASS, db=TDB)
