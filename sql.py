af6ea2fa1cae7fb83719bc585a3c13eea9bedde0
Touya Akira <none@none.org>
2013-01-23 10:05:53 +0000

    format fixes

--- sql.py
+++ sql.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python
-"""Simple irc to mysql interface
-to find, add and remove entries
-to the tor authentification db.
-
-v1.0
-author: Touya Akira
-"""
+"""TorAuth Module
+v1.1 Touya Akira"""
 
 import MySQLdb;
 
-CCHANNEL = ""
-TTABLE = ''
-THOST = ''
-TUSER = ''
-TPASS = ''
-TDB = ''
+CCHANNEL = "OPER CHANNEL"
+TTABLE = 'DB TABLE'
+THOST = 'DB HOST'
+TUSER = 'DB USER'
+TPASS = 'DB PASS'
+TDB = 'DB NAME'
 
 def thelp(phenny, input):
 	phenny.say("Usage:")
@@ -26,17 +21,23 @@ thelp.commands = ['help']
 def searchi(phenny, input):
 	if (input.sender != CCHANNEL): return
 	else:
-		db = MySQLdb.connect(host=THOST, user=TUSER, passwd=TPASS, db=TDB)
-		cursor = db.cursor()
-		sql = 'SELECT * FROM ' +TTABLE+' WHERE ident = "'+input.group(2)+'"'
-		phenny.say(sql)
-		cursor.execute(sql)
-		results = cursor.fetchall()
-		for row in results:
-			rident = row[0]
-			rhash = row[1]
-			phenny.say(rident+" "+rhash)
-		db.close()
+		if input.group(2) == "*":
+			phenny.say("\002TOR AUTH: \002Err, nope. Not gonna flood the channel. Please use the website to list all users, kthx.")
+		else:	
+			db = MySQLdb.connect(host=THOST, user=TUSER, passwd=TPASS, db=TDB)
+			cursor = db.cursor()
+			sql = 'SELECT * FROM ' +TTABLE+' WHERE ident = "'+input.group(2)+'"'
+			phenny.say("\002TOR AUTH: \002sending: "+sql)
+			cursor.execute(sql)
+			results = cursor.fetchall()
+			i = 0;
+			for row in results:
+				i += 1
+				rident = row[0]
+				rhash = row[1]
+				phenny.say("\002TOR AUTH: \002"+rident+" "+rhash)
+			phenny.say("\002TOR AUTH: \002All matches displayed.")
+			db.close()
 searchi.commands = ['find']		
 
 def addi(phenny, input):
@@ -44,15 +45,16 @@ def addi(phenny, input):
 	else:
 		query = input.group(2).split()
 		if len(query[1]) != 64:
-			phenny.say("Hash length does not match. Are you sure you have entered a sha256 hash?")
+			phenny.say("\002TOR AUTH: \002Hash length does not match. Are you sure you have entered a sha256 hash?")
 			return
 		else:
 			db = MySQLdb.connect(host=THOST, user=TUSER, passwd=TPASS, db=TDB)
 			cursor = db.cursor()
 			sql = 'INSERT INTO '+TTABLE+' (ident, hash) VALUES ("'+query[0]+'", "'+query[1]+'")'
-			phenny.say(sql)
+			phenny.say("\002TOR AUTH: \002sending: "+sql)
 			cursor.execute(sql)
 			db.close()
+			phenny.say("\002TOR AUTH: \002User "+query[0]+" added.")
 addi.commands = ['add']
 
 def deli(phenny, input):
@@ -65,8 +67,9 @@ def deli(phenny, input):
 			db = MySQLdb.connect(host=THOST, user=TUSER, passwd=TPASS, db=TDB)
 			cursor = db.cursor()
 			sql = 'DELETE FROM '+TTABLE+' WHERE hash = "'+input.group(2)+'"'
-			phenny.say(sql)
+			phenny.say("\002TOR AUTH: \002sending: "+sql)
 			cursor.execute(sql)
 			db.close()
+			phenny.say("\002TOR AUTH: \002User with hash "+input.group(2)+" deleted.")
 deli.commands = ['del']
 
